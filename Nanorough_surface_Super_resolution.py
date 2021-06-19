6092641ae734d6267473c3dadbbf5c9603a84543
billsioros <billsioros97@gmail.com>
2021-06-19 19:54:19 +0300

    fix: sync `py` and `ipynb` versions

--- Nanorough_surface_Super_resolution.py
+++ Nanorough_surface_Super_resolution.py
@@ -13,13 +13,13 @@
 #     name: python3
 # ---
 
-# + [markdown] colab_type="text" id="view-in-github"
+# + [markdown] id="view-in-github" colab_type="text"
 # <a href="https://colab.research.google.com/github/billsioros/thesis/blob/master/Nanorough_surface_Super_resolution.ipynb" target="_parent"><img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/></a>
 
-# + [markdown] id="_XIigYPTAMAH"
+# + [markdown] id="cce26209"
 # # ✔️ Prerequisites
 
-# + [markdown] id="sv3lJtoU-aoQ"
+# + [markdown] id="affab565"
 # First of all we need to take care of a few **prerequisites**, most notably:
 #
 # - Install the various pip modules that we will be using.
@@ -30,33 +30,33 @@
 #   - The available hardware backend. GPU utilization is preferable, as it results in higher complition time.
 # - `(Optionally)` Mount Google Drive, where we can load our dataset from.
 
-# + [markdown] id="ehWvZSwBXSXI"
+# + [markdown] id="16a902e2"
 # ## Installing [graphviz](https://graphviz.org/) & [libgraphviz-dev](https://packages.debian.org/jessie/libgraphviz-dev)
 
-# + [markdown] id="12JZQTC3YaxN"
+# + [markdown] id="8c0cdd85"
 # The aforementioned packages are required by [PyINSECT](https://github.com/billsioros/PyINSECT/tree/implementing-HPGs) and more specifically its graph plotting methods.
 
-# + cellView="code" colab={"base_uri": "https://localhost:8080/"} id="ANi0d4-f57Qc" outputId="908da9e3-f467-4c34-815e-29b23b68b8c8"
+# + cellView="code" colab={"base_uri": "https://localhost:8080/"} id="919734ca" outputId="fd43fbde-ade3-4225-c24e-e950d0baab55"
 # !sudo apt-get install graphviz libgraphviz-dev
 
-# + [markdown] id="f1mdRrYGM60R"
+# + [markdown] id="7f5668f4"
 # ## Installing the required `pip` modules
 
-# + [markdown] id="l5FLZ3mv_ho8"
+# + [markdown] id="aebad6f1"
 # - [torch](https://pytorch.org/) is our machine learning framework of choice.
 # - [numpy](https://numpy.org/), [sympy](https://www.sympy.org/en/index.html) and [scipy](https://www.scipy.org/) are used to in the context of nanorough surface generation.
 # - [plotly](https://plotly.com/) (which requires [pandas](https://pandas.pydata.org/)) as well as [matplotlib](https://matplotlib.org/) are used in order to plot various graphs.
 
-# + cellView="code" colab={"base_uri": "https://localhost:8080/"} id="5fDiW2N-M60S" outputId="db16de1e-3558-495d-f0a0-0d672f11eb37"
+# + cellView="code" colab={"base_uri": "https://localhost:8080/"} id="1057687b" outputId="15f921e0-f921-47ab-b4e2-8a355c422e54"
 # !pip install torch numpy sympy scipy plotly pandas sklearn matplotlib==3.1.1 git+https://github.com/billsioros/PyINSECT.git@FEATURE_Implementing_HPGraphCollector git+https://github.com/billsioros/thesis.git
 
-# + [markdown] id="WktNsUHMAS63"
+# + [markdown] id="0192c059"
 # ## Initializing (a.k.a `Seeding`) the Random Number Generator(s)
 
-# + [markdown] id="-1mLAArpLX7A"
+# + [markdown] id="fa44f756"
 # We are required to seed various random number generation engines, so that our experiments can be replicated on a later date.
 
-# + cellView="code" id="iw9sr-9MA-dI"
+# + cellView="code" id="5daee8da"
 SEED = 1234
 
 import os
@@ -64,7 +64,7 @@ import random
 
 import numpy as np
 
-# + cellView="code" id="QMnousxlAbPV"
+# + cellView="code" id="4d6c30c9"
 import torch
 
 if SEED is not None:
@@ -75,21 +75,21 @@ if SEED is not None:
     torch.backends.cudnn.deterministic = True
     os.environ["PYTHONHASHSEED"] = str(SEED)
 
-# + [markdown] id="zxhexKDEM60T"
+# + [markdown] id="19e0a6d0"
 # ## Determining the Current Working Directory
 
-# + cellView="code" id="1oRdJanBM60T"
+# + cellView="code" id="945a9ccd"
 from pathlib import Path
 
 BASE_DIR = Path.cwd()
 
-# + [markdown] id="ptZs2vpwNwJg"
+# + [markdown] id="94c4d99f"
 # ## Mounting Google Drive
 
-# + cellView="code" id="NXUKAqjuOvpK"
+# + cellView="code" id="12dcaea0"
 GDRIVE_DIR = BASE_DIR / "drive"
 
-# + cellView="code" colab={"base_uri": "https://localhost:8080/"} id="musKsMGXNyQM" outputId="f98fd15d-99ee-4beb-e25a-82069ff8bf49"
+# + cellView="code" colab={"base_uri": "https://localhost:8080/"} id="e24f0051" outputId="63d677c9-6087-4045-8fa7-bdce4181473d"
 try:
     from google.colab import drive
 
@@ -97,64 +97,64 @@ try:
 except ImportError:
     pass
 
-# + [markdown] id="JflfjrdmDqL0"
+# + [markdown] id="6fce8a2a"
 # ## Determining available backend
 
-# + [markdown] id="x1IGaiyqYIoI"
+# + [markdown] id="5ca328f8"
 # By default, we are going to be utilizing the available CPU backend, if no GPU is available.
 
-# + cellView="code" id="ykoV8K_vDurq"
+# + cellView="code" id="ebabd4a5"
 device = "cpu"
 if torch.cuda.is_available():
     device = "cuda:0"
 
-# + cellView="code" id="NYf2R4B1VgvI"
+# + cellView="code" id="520ba5c1"
 device = torch.device(device)
 
-# + [markdown] id="oQ2HamjCaPFw"
+# + [markdown] id="64c09dfe"
 # ## Configuring our Loggers
 
-# + cellView="code" id="FDPrOnIyaVQV"
+# + cellView="code" id="5eea1ad8"
 import logging
 
 logging.basicConfig(
     format="[%(asctime)s] %(levelname)s:%(name)s: %(message)s", level=logging.CRITICAL
 )
 
-# + id="KXIF2OMgV1rj"
+# + id="b45114ef"
 logger = logging.getLogger()
-# -
 
+# + [markdown] id="60d78d1e"
 # # 🙃 A naive-approach
 
-# + [markdown] id="n8K2Yt4Yygnp"
+# + [markdown] id="ec374650"
 # ## Instantiating the **Generator** and the **Discriminator** Networks
 
-# + cellView="code" id="EfhGL9wz_igq"
+# + cellView="code" id="d6594cb1" outputId="ef0da838-1378-4614-8c5a-ead42de77ebb" colab={"base_uri": "https://localhost:8080/", "height": 333}
 from roughml.models import PerceptronGenerator
 
 generator = PerceptronGenerator.from_device(device)
 
-# + cellView="code" colab={"base_uri": "https://localhost:8080/"} id="oqW-HWLlqW0j" outputId="617dc2ad-e900-4f46-e0a1-ec1c7e9b6f58"
+# + cellView="code" id="4bff3a44"
 generator
 
-# + cellView="code" id="SllxLU-zqqfO"
+# + cellView="code" id="cac059ee"
 from roughml.models import PerceptronDiscriminator
 
 discriminator = PerceptronDiscriminator.from_generator(generator, device=device)
 
-# + cellView="code" colab={"base_uri": "https://localhost:8080/"} id="XSirWsR3qZVq" outputId="b981661f-2537-4e03-9901-675c6019f16e"
+# + cellView="code" id="64022987"
 discriminator
 
-# + [markdown] id="_Dev3j37tn3x"
+# + [markdown] id="eb813599"
 # ## Training
 
-# + cellView="code" id="nhk6EKKvYcSo"
+# + cellView="code" id="8a131c21"
 from torch.nn import BCELoss
 
 criterion = BCELoss().to(device)
 
-# + cellView="code" id="4O4QebF6US_N"
+# + cellView="code" id="90443487"
 from pathlib import Path
 
 CHECKPOINT_DIR = BASE_DIR / "checkpoint"
@@ -163,7 +163,7 @@ CHECKPOINT_DIR.mkdir(parents=True, exist_ok=True)
 from roughml.content.loss import NGramGraphContentLoss
 from roughml.data.transforms import Flatten, To
 
-# + cellView="code" id="ky0tb0YFJQoG"
+# + cellView="code" id="63114157"
 from roughml.training.flow import TrainingFlow
 from roughml.training.manager import per_epoch
 
@@ -192,35 +192,35 @@ training_flow = TrainingFlow(
     },
 )
 
-# + colab={"base_uri": "https://localhost:8080/", "height": 573} id="iC8dJd352UCq" outputId="c45e8835-d52f-446d-bfc1-eca83af9e53b"
+# + id="836ed418"
 training_flow(generator, discriminator)
-# -
 
+# + [markdown] id="fe589c1a"
 # # 😎 A CNN based approach
 
-# + [markdown] id="pz3FhXtfKfVS"
+# + [markdown] id="b16a4946"
 # ## Instantiating the **Generator** and the **Discriminator** Networks
 
-# + cellView="code" id="FfZnXKRwKfVT"
+# + cellView="code" id="e301a7a0"
 from src.roughml.models import CNNGenerator
 
 generator = CNNGenerator.from_device(device)
 
-# + cellView="code" colab={"base_uri": "https://localhost:8080/"} id="wCibc_0iqc3j" outputId="b05a34d2-4cbf-436f-c2f7-9ff31e19ebf7"
+# + cellView="code" id="36ffe487"
 generator
 
-# + cellView="code" id="_S2URr5kKfVT"
+# + cellView="code" id="5a8a9aad"
 from src.roughml.models import CNNDiscriminator
 
 discriminator = CNNDiscriminator.from_device(device)
 
-# + cellView="code" colab={"base_uri": "https://localhost:8080/"} id="A3zp9MZpqdem" outputId="fca9f23d-8b6d-46c7-db40-ec3baf785806"
+# + cellView="code" id="1d0a4a40"
 discriminator
 
-# + [markdown] id="oUyCo282KWod"
+# + [markdown] id="7bbbcc22"
 # ## Training
 
-# + cellView="code" id="Pp0NBoc4KWoe"
+# + cellView="code" id="45778b4c"
 from torch.nn import BCELoss
 
 criterion = BCELoss().to(device)
@@ -228,7 +228,7 @@ criterion = BCELoss().to(device)
 from roughml.content.loss import ArrayGraph2DContentLoss
 from roughml.data.transforms import To, View
 
-# + cellView="code" id="UrJuNU0tKWoe"
+# + cellView="code" id="82fb12f6"
 from roughml.training.flow import TrainingFlow
 from roughml.training.manager import per_epoch
 
@@ -257,5 +257,5 @@ training_flow = TrainingFlow(
     },
 )
 
-# + colab={"base_uri": "https://localhost:8080/", "height": 573} id="1m2idVrat7_M" outputId="e698e816-34e0-4ad1-a1f7-1f61a9a04dcf"
+# + id="3c4cdce1"
 training_flow(generator, discriminator)
