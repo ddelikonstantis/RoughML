cbd4eb064bf5aa1cd27f2b3e6c0ccd02bb5921e7
Dimitrios Delikonstantis <dim.delikonstantis@hotmail.com>
2022-03-29 20:55:43 +0300

    Update Nanorough_surface_Super_resolution.py

--- Nanorough_surface_Super_resolution.py
+++ Nanorough_surface_Super_resolution.py
@@ -159,6 +159,7 @@ if SEED is not None:
 
 # + cellView="code" id="520ba5c1"
 device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+print(device)
 print(f"Is CUDA supported? {torch.cuda.is_available()}")
 print(f"CUDA version: {torch.version.cuda}")
 # Storing ID of current CUDA device
