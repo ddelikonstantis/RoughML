b7d3417e1711d5cda0c303f699d421195585946b
Dimitrios Delikonstantis <dim.delikonstantis@hotmail.com>
2022-04-03 17:04:08 +0300

    Update Nanorough_surface_Super_resolution.py

--- Nanorough_surface_Super_resolution.py
+++ Nanorough_surface_Super_resolution.py
@@ -1,3 +1,4 @@
+
 # -*- coding: utf-8 -*-
 # ---
 # jupyter:
@@ -262,14 +263,14 @@ training_flow = TrainingFlow(
             "train_epoch": per_epoch,
             "log_every_n": 10,
             "criterion": {"instance": criterion},
-            "n_epochs": 10,
+            "n_epochs": 100,
             "train_ratio": 0.8,
             "optimizer": {
                 "type": Adam,
-                "params": {"lr": 0.1, "weight_decay": 0},
+                "params": {"lr": 0.001, "betas": (0.5, 0.999)},
             },
             "dataloader": {
-                "batch_size": 256,
+                "batch_size": 32,
                 "shuffle": True,
                 "num_workers": 0,
             },
@@ -288,7 +289,7 @@ training_flow = TrainingFlow(
             load_multiple_datasets_from_pt,
             DATASET_DIR,
             transforms=[To(device), View(1, 128, 128)],
-            limit=(2, 10),
+            limit=None,
         )
     },
     animation={
@@ -358,7 +359,7 @@ training_flow = TrainingFlow(
             "train_epoch": per_epoch,
             "log_every_n": 10,
             "criterion": {"instance": criterion},
-            "n_epochs": 10,
+            "n_epochs": 200,
             "train_ratio": 0.8,
             "optimizer": {
                 "type": Adam,
@@ -384,7 +385,7 @@ training_flow = TrainingFlow(
             load_multiple_datasets_from_pt,
             DATASET_DIR,
             transforms=[To(device), View(1, 128, 128)],
-            limit=(2, 10),
+            limit=None,
         )
     },
     animation={
