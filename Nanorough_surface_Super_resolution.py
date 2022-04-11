fdb4bd4f4b9a3a6f34734b67adecf455e7e8a7dc
Dimitrios Delikonstantis <dim.delikonstantis@hotmail.com>
2022-04-11 19:53:47 +0300

    torch device updated

--- Nanorough_surface_Super_resolution.py
+++ Nanorough_surface_Super_resolution.py
@@ -1,4 +1,3 @@
-
 # -*- coding: utf-8 -*-
 # ---
 # jupyter:
@@ -159,7 +158,7 @@ if SEED is not None:
 # By default, we are going to be utilizing the available CPU backend, if no GPU is available.
 
 # + cellView="code" id="520ba5c1"
-device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # -
 
 
@@ -252,18 +251,18 @@ training_flow = TrainingFlow(
         "manager": {
             "benchmark": True,
             # Uncomment if you want to enable checkpointing
-            "checkpoint": {"multiple": True},
+            # "checkpoint": {"multiple": True},
             "train_epoch": per_epoch,
             "log_every_n": 10,
             "criterion": {"instance": criterion},
-            "n_epochs": 100,
+            "n_epochs": 10,
             "train_ratio": 0.8,
             "optimizer": {
                 "type": Adam,
-                "params": {"lr": 0.001, "betas": (0.5, 0.999)},
+                "params": {"lr": 0.1, "weight_decay": 0},
             },
             "dataloader": {
-                "batch_size": 32,
+                "batch_size": 256,
                 "shuffle": True,
                 "num_workers": 0,
             },
@@ -275,14 +274,14 @@ training_flow = TrainingFlow(
     NGramGraphLoss={
         "type": ArrayGraph2DContentLoss,
         # Uncomment if you want to enable checkpointing
-        "cache": "n_gram_graph_content_loss.pkl",
+        # "cache": "n_gram_graph_content_loss.pkl",
     },
     data={
         "loader": functools.partial(
             load_multiple_datasets_from_pt,
             DATASET_DIR,
             transforms=[To(device), View(1, 128, 128)],
-            limit=None,
+            limit=(2, 10),
         )
     },
     animation={
@@ -352,7 +351,7 @@ training_flow = TrainingFlow(
             "train_epoch": per_epoch,
             "log_every_n": 10,
             "criterion": {"instance": criterion},
-            "n_epochs": 200,
+            "n_epochs": 10,
             "train_ratio": 0.8,
             "optimizer": {
                 "type": Adam,
@@ -378,7 +377,7 @@ training_flow = TrainingFlow(
             load_multiple_datasets_from_pt,
             DATASET_DIR,
             transforms=[To(device), View(1, 128, 128)],
-            limit=None,
+            limit=(2, 10),
         )
     },
     animation={
