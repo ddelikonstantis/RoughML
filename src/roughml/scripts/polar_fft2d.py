6324b2ec8c3d22987d9c62a299a9279b1d196ca1
Dimitrios Delikonstantis <dim.delikonstantis@hotmail.com>
2022-05-03 12:25:23 +0300

    Update polar_fft2d.py

--- src/roughml/scripts/polar_fft2d.py
+++ src/roughml/scripts/polar_fft2d.py
@@ -106,7 +106,8 @@ def polar_FFT2d(*args):
     
     # get average polar FFT2d values for all images
     polar_fft_total_mean = np.mean(polar_fft_total, axis=0)
-    polar_fft_total_mean = np.column_stack((np.full(shape=polar_fft_total_mean.shape, fill_value=float(alpha), order='F'), polar_fft_total_mean))
+    # polar_fft_total_mean = np.column_stack((np.full(shape=polar_fft_total_mean.shape, fill_value=float(alpha), order='F'), polar_fft_total_mean))
+    polar_fft_total_mean = np.column_stack((polar_fft[:,0], polar_fft_total_mean))
     # create csv file with mean vector values
     df = pd.DataFrame(polar_fft_total_mean, columns = ['Alpha', 'Polar FFT2d values'])
     df_filename = '/polar_fft2d_' + alpha + '.csv'
@@ -126,47 +127,49 @@ if __name__ == "__main__":
     # # parser.add_argument('image5', help = 'directory of fifth image to compare')
     # args = parser.parse_args()
 
-    # arg1=r"Output\Alpha effect\Standard\dataset_1000_128_03_00_03_04_04_1.00\CNNGenerator_CNNDiscriminator\2022_04_17_14_39_45_776175\Plots\grayscale\fake_00.png"
-    # arg2=r"Output\Alpha effect\Standard\dataset_1000_128_03_00_03_04_04_1.00\CNNGenerator_CNNDiscriminator\2022_04_17_14_39_45_776175\Plots\grayscale\fake_01.png"
-    # arg3=r"Output\Alpha effect\Standard\dataset_1000_128_03_00_03_04_04_1.00\CNNGenerator_CNNDiscriminator\2022_04_17_14_39_45_776175\Plots\grayscale\fake_02.png"
-    # arg4=r"Output\Alpha effect\Standard\dataset_1000_128_03_00_03_04_04_1.00\CNNGenerator_CNNDiscriminator\2022_04_17_14_39_45_776175\Plots\grayscale\fake_03.png"
-    # arg5=r"Output\Alpha effect\Standard\dataset_1000_128_03_00_03_04_04_1.00\CNNGenerator_CNNDiscriminator\2022_04_17_14_39_45_776175\Plots\grayscale\fake_04.png"
-    # arg6=r"Output\Alpha effect\Standard\dataset_1000_128_03_00_03_04_04_1.00\CNNGenerator_CNNDiscriminator\2022_04_17_14_39_45_776175\Plots\grayscale\fake_05.png"
+    arg1=r"Output\Alpha effect\Standard\dataset_1000_128_03_00_03_04_04_0.90\CNNGenerator_CNNDiscriminator\2022_04_17_14_29_03_199460\Plots\grayscale\fake_00.png"
+    arg2=r"Output\Alpha effect\Standard\dataset_1000_128_03_00_03_04_04_0.90\CNNGenerator_CNNDiscriminator\2022_04_17_14_29_03_199460\Plots\grayscale\fake_01.png"
+    arg3=r"Output\Alpha effect\Standard\dataset_1000_128_03_00_03_04_04_0.90\CNNGenerator_CNNDiscriminator\2022_04_17_14_29_03_199460\Plots\grayscale\fake_02.png"
+    arg4=r"Output\Alpha effect\Standard\dataset_1000_128_03_00_03_04_04_0.90\CNNGenerator_CNNDiscriminator\2022_04_17_14_29_03_199460\Plots\grayscale\fake_03.png"
+    arg5=r"Output\Alpha effect\Standard\dataset_1000_128_03_00_03_04_04_0.90\CNNGenerator_CNNDiscriminator\2022_04_17_14_29_03_199460\Plots\grayscale\fake_04.png"
+    arg6=r"Output\Alpha effect\Standard\dataset_1000_128_03_00_03_04_04_0.90\CNNGenerator_CNNDiscriminator\2022_04_17_14_29_03_199460\Plots\grayscale\fake_05.png"
 
-    # polar_FFT2d(arg1, arg2, arg3, arg4, arg5, arg6)
+    polar_FFT2d(arg1, arg2, arg3, arg4, arg5, arg6)
 
-    df1 = pd.read_csv(r'src\roughml\scripts\output\Alpha effect\0.5\polar_fft2d_0.5.csv', header=0)
-    df1 = np.array(df1)
-    df2 = pd.read_csv(r'src\roughml\scripts\output\Alpha effect\0.6\polar_fft2d_0.6.csv', header=0)
-    df2 = np.array(df2)
-    df3 = pd.read_csv(r'src\roughml\scripts\output\Alpha effect\0.7\polar_fft2d_0.7.csv', header=0)
-    df3 = np.array(df3)
-    df4 = pd.read_csv(r'src\roughml\scripts\output\Alpha effect\0.8\polar_fft2d_0.8.csv', header=0)
-    df4 = np.array(df4)
-    df5 = pd.read_csv(r'src\roughml\scripts\output\Alpha effect\0.9\polar_fft2d_0.9.csv', header=0)
-    df5 = np.array(df5)
-    df6 = pd.read_csv(r'src\roughml\scripts\output\Alpha effect\1.0\polar_fft2d_1.0.csv', header=0)
-    df6 = np.array(df6)
+    # df1 = pd.read_csv(r'src\roughml\scripts\output\Alpha effect new\0.5\polar_fft2d_0.5.csv', header=0)
+    # df1 = np.array(df1)
+    # df2 = pd.read_csv(r'src\roughml\scripts\output\Alpha effect new\0.6\polar_fft2d_0.6.csv', header=0)
+    # df2 = np.array(df2)
+    # df3 = pd.read_csv(r'src\roughml\scripts\output\Alpha effect new\0.7\polar_fft2d_0.7.csv', header=0)
+    # df3 = np.array(df3)
+    # df4 = pd.read_csv(r'src\roughml\scripts\output\Alpha effect new\0.8\polar_fft2d_0.8.csv', header=0)
+    # df4 = np.array(df4)
+    # df5 = pd.read_csv(r'src\roughml\scripts\output\Alpha effect new\0.9\polar_fft2d_0.9.csv', header=0)
+    # df5 = np.array(df5)
+    # df6 = pd.read_csv(r'src\roughml\scripts\output\Alpha effect new\1.0\polar_fft2d_1.0.csv', header=0)
+    # df6 = np.array(df6)
     
 
-    df_mean_total = []
-    df_mean_total = np.concatenate((df1, df2, df3, df4, df5, df6))
+    # # df_mean_total = []
+    # # df_mean_total = np.concatenate((df1, df2, df3, df4, df5, df6))
 
-    path = os.path.join(os.path.dirname( __file__ ), 'output/').replace('\\', '/')
-    try:
-        os.mkdir(path)
-    except FileExistsError:
-        # directory already exists
-        pass
+    # df_mean_total = df6
+
+    # path = os.path.join(os.path.dirname( __file__ ), 'output/').replace('\\', '/')
+    # try:
+    #     os.mkdir(path)
+    # except FileExistsError:
+    #     # directory already exists
+    #     pass
     
-    df_total = pd.DataFrame(df_mean_total)
-    df_filename = '/polar_fft2d_total.csv'
-    df_total.to_csv(path + df_filename, index=None, header=None)
+    # df_total = pd.DataFrame(df_mean_total)
+    # df_filename = '/polar_fft2d_total.csv'
+    # df_total.to_csv(path + df_filename, index=None, header=None)
 
-    coef, p = spearmanr(df_mean_total)
-    # print('Spearmans coefficient: ', coef, '\n', 'p-value: ', p)
-    spr = np.array([[coef, p]])
-    df_spr = pd.DataFrame(spr, columns = ['Spearmans coefficient', 'p-value'])
-    df_spr_filename = '/Spearmans rank correlation coefficient.csv'
-    df_spr.to_csv(path + df_spr_filename, index=None)
+    # coef, p = spearmanr(df_mean_total)
+    # # print('Spearmans coefficient: ', coef, '\n', 'p-value: ', p)
+    # spr = np.array([[coef, p]])
+    # df_spr = pd.DataFrame(spr, columns = ['Spearmans coefficient', 'p-value'])
+    # df_spr_filename = '/Spearmans rank correlation coefficient.csv'
+    # df_spr.to_csv(path + df_spr_filename, index=None)
 
