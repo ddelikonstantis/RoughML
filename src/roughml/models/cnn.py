50d87cf9850e360a4e1cc7272b9247755285e7a5
Dimitris Delikonstantis <dim.delikonstantis@hotmail.com>
2023-02-16 07:59:41 +0200

    Update cnn.py

--- src/roughml/models/cnn.py
+++ src/roughml/models/cnn.py
@@ -18,9 +18,9 @@ class CNNBase(Base):
     def initialize_weights(cls, model):
         classname = model.__class__.__name__
         if classname.find("Conv") != -1:
-            nn.init.normal_(model.weight.data, 0.0, 0.02)
+            nn.init.normal_(model.weight.data, 0.0, 0.2)
         elif classname.find("BatchNorm") != -1:
-            nn.init.normal_(model.weight.data, 1.0, 0.02)
+            nn.init.normal_(model.weight.data, 1.0, 0.2)
             nn.init.constant_(model.bias.data, 0)
 
     @classmethod
